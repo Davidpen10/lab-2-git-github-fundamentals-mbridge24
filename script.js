b5e12699bf5e1451fea96ff09ce831ea7110bd3a
Davidpen10 <david.penafiel1@marist.edu>
2023-09-20 09:53:52 -0400

    javascript code

---
+++ script.js
@@ -0,0 +1,14 @@
+// This function clears all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+// This function displays the values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+// This function evaluates the expression and returns the result
+function calculate() {
+    let expression = document.getElementById("result").value;
+    let answer = eval(expression);
+    document.getElementById("result").value = answer;
+}
